18de14fd10c800b54203daa05aa7ca4ff1536f7e
Rajat Keshri <rkeshri98@gmail.com>
2023-02-26 00:56:45 +0000

    updated to fetch data from ghs

--- index.ts
+++ index.ts
@@ -22,14 +22,15 @@ app.post('/runner', jsonParser, async (req: Request, res: Response)=>{
     const workflowJob = req.body.workflow_job;
     const org = req.body.org_name;
     const workflowRepo = req.body.repository;
-    const authToken = req.body.token;
+    //const authToken = req.body.token; //jwt token
     const repoURL = workflowRepo.html_url;
 
     //const org = repoURL.split("/").slice(-2, -1)[0];
     const repoName = repoURL.split("/").at(-1);
     
-    console.log(authToken, org, repoName);
-    var executeDocker = await child_process.exec('sudo docker run --rm actions-image '+ repoURL + ' ' + org + ' ' + repoName + ' ' + authToken,  
+    //console.log(authToken, org, repoName);
+    //var executeDocker = await child_process.exec('sudo docker run --rm actions-image '+ repoURL + ' ' + org + ' ' + repoName + ' ' + authToken,  
+    var executeDocker = await child_process.exec('sudo docker run --rm actions-image '+ repoURL + ' ' + org + ' ' + repoName,  
                         (err, stdout, stderr) => {
                           if (err) {
                             console.error(err)
--- runner/entrypoint.sh
+++ runner/entrypoint.sh
@@ -6,20 +6,24 @@ AUTH_TOKEN=$4
 NAME=$5
 
 
-cleanup() {
-    token=$(curl -s -XPOST -H "authorization: token ${AUTH_TOKEN}" \
-        https://api.github.com/repos/${ORG}/${REPONAME}/actions/runners/registration-token |\
-        jq -r .token)
-    ./config.sh remove --token $token
-}
+#cleanup() {
+#    token=$(curl -s -XPOST -H "authorization: token ${AUTH_TOKEN}" \
+#        https://api.github.com/repos/${ORG}/${REPONAME}/actions/runners/registration-token |\
+#        jq -r .token)
+#    ./config.sh remove --token $token
+#}
+
+#token=$(curl -s -XPOST \
+#    -H "authorization: token ${AUTH_TOKEN}" \
+#    https://api.github.com/repos/${ORG}/${REPONAME}/actions/runners/registration-token |\
+#    jq -r .token)
 
 token=$(curl -s -XPOST \
-    -H "authorization: token ${AUTH_TOKEN}" \
-    https://api.github.com/repos/${ORG}/${REPONAME}/actions/runners/registration-token |\
+    https://localhost:8181/runnertoken/${ORG}/${REPONAME} |\
     jq -r .token)
 
- ./config.sh --url ${URL} --token ${token} --work _work --name ${NAME}
+./config.sh --url ${URL} --token ${token} --work _work --name ${NAME} --emphemeral
 
 ./run.sh --once
 
-cleanup
+#cleanup
